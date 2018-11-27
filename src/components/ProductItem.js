4204bf7e81fe67fc290c3344160108b15ef3ad53
Isaac <isaacalves@gmail.com>
2018-11-27 09:25:51 +0000

    use state hook in product item

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,33 +1,23 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Collapse } from 'react-collapse';
 
-class ProductItem extends PureComponent {
-  state = {
-    isActive: false
-  };
-
-  render() {
-    let { isActive } = this.state;
-    return (
-      <>
-        <div className="ProductList__item">
-          <div
-            className="ProductList__item__title"
-            onClick={() => this.setState({ isActive: !this.state.isActive })}
-          >
-            <strong>{this.props.title}</strong>
-          </div>
-          <Collapse isOpened={isActive}>
-            <div className="ProductList__item__description">
-              {this.props.description}
-            </div>
-          </Collapse>
-        </div>
-      </>
-    );
-  }
-}
+const ProductItem = ({ title, description }) => {
+  const [isActive, toggleActive] = useState(false);
+  return (
+    <div className="ProductList__item">
+      <div
+        className="ProductList__item__title"
+        onClick={() => toggleActive(isActive => !isActive)}
+      >
+        <strong>{title}</strong>
+      </div>
+      <Collapse isOpened={isActive}>
+        <div className="ProductList__item__description">{description}</div>
+      </Collapse>
+    </div>
+  );
+};
 
 ProductItem.propTypes = {
   title: PropTypes.string.isRequired,
